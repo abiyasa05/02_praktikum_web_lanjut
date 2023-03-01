8d5b831d639a4a0f6ed09ec1d81b2bf1573a1811
abiyasa05 <abiyasap24@gmail.com>
2023-03-01 08:16:28 +0700

    Baru Selamat Datang

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/{id}', function () {
+Route::get('/', function () {
 echo "<h1>Selamat Datang</h1>";
 });
 
